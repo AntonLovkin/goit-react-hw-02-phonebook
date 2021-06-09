03666f4152fcf9dcebc3aeeca14b1d21c6c1bb47
AntonLovkin <anton28111987@gmail.com>
2021-06-09 23:15:09 +0300

    step #1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
+import { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
+
+  handleChange = (evt) => {
+    this.setState({
+      name: evt.currentTarget.value,
+    });
+  };
+
+  addContact = (name) => {
+    console.log(name);
+
+    const contact = {
+      id: uuidv4(),
+      name,
+    };
+    this.setState((prevState) => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    // console.log(this.state);
+
+    this.addContact(this.state.name);
+
+    this.setState({ name: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Phonebook</h2>
+        <label>
+          Name
+          <input
+            value={this.state.name}
+            onChange={this.handleChange}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title=" "
+            required
+          />
+        </label>
+        <button type="submit">Add contact</button>
+
+        <ul>Contacts</ul>
+        {console.log(this.state.contacts)}
+        {this.state.contacts.map((contact) => (
+          <li key={contact.id}>{contact.name}</li>
+        ))}
+      </form>
+    );
+  }
 }
 
 export default App;
